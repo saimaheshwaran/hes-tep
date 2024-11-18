5bbfa31605e50a0ca2b82e9fa3e5dbff9966bbfd
saimaheshwaran <saimaheshwaran@outlook.com>
2024-11-18 10:24:51 -0500

    First Push
    Initial code

---
+++ src/main/java/org/sm/api/config/RestConfig.java
@@ -0,0 +1,8 @@
+package org.sm.api.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RestConfig {
+    private static final Logger logger = LoggerFactory.getLogger(RestConfig.class);
+}
